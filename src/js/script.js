da2f4858032b4de6985654b50be1d18ab0370ac4
LevaniMigriauli <levanimigriauli16@gmail.com>
2022-03-08 20:12:17 +0400

    Starting a project

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const demo = () =>
+  "Webpack Boilerplate v5.11.0 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.";
+
+// eslint-disable-next-line no-console
+console.log(demo());
