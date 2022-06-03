b384583776565d0828d235df93590d00ca60f578
LevaniMigriauli <levanimigriauli16@gmail.com>
2022-06-03 18:49:01 +0400

    working on adding taskd

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,12 @@
 "use strict";
 
-const listContainer = document.getElementById("todo-list");
-const input = document.getElementById("input");
-const btnAdd = document.getElementById("button-add");
-const btnDelete = document.querySelector("todo-list__task-item::after");
-
-// const btnDelete = document.createElement("button");
-// const btnDel = (btnDelete.innerText = "delete");
-
-const taskList = [];
-console.log(taskList);
+const taskList = document.querySelector(".task-list");
+const btnAdd = document.querySelector(".btn-add");
+const input = document.querySelector(".task-input");
 
 btnAdd.addEventListener("click", function () {
-  //   document.body.append(document.createElement("li"));
-  //   let task = listContainer.append(document.createElement("li"));
-  taskList.push(input.value);
-  // console.log(input.value);
-  const task = document.createElement("li");
-  task.classList.add("todo-list__task-item");
-  for (let l = 0; l < taskList.length; l++) {
-    listContainer.appendChild(task).textContent = taskList[l];
-    // listContainer.appendChild(task).textContent = "task";
-  }
+  const taskItem = document.createElement("li");
+  const taskContent = input.value;
+  console.log(taskContent);
+  taskList.appendChild(taskItem).textContent = taskContent;
 });
-
-// btnDelete.addEventListener("click", function (e) {
-//   taskList.pop(e);
-//   // for (let l = 0; l < taskList.length; l++) {}
-//   console.log(e);
-//   console.log(taskList);
-// });
-
-console.log(btnDelete);
