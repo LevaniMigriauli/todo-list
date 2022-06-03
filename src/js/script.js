436f67c4915a94c9459849240a666a5a53086de2
LevaniMigriauli <levanimigriauli16@gmail.com>
2022-06-04 03:11:33 +0400

    starting from complete scratch

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,10 @@ const input = document.querySelector(".task-input");
 btnAdd.addEventListener("click", function () {
   const taskItem = document.createElement("li");
   const taskContent = input.value;
-  console.log(taskContent);
   taskList.appendChild(taskItem).textContent = taskContent;
+  taskItem.classList.add("button-edit");
+  taskItem.classList.add("button-delete");
+  taskItem.classList.add("task-item");
 });
+
+const btnEdit = document.querySelector;
